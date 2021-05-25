f8dcfb3b2fd930cb8cd1b23eea92303be252809e
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-25 20:57:20 +0700

    add theme

---
+++ lib/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color blue = Color(0xff407BFF);
+Color black = Color(0xff1e272e);
+Color grey = Color(0xff808e9b);
+Color lightGrey = Color(0xffF3F3F3);
+
+TextStyle title =
+    TextStyle(fontSize: 26, fontWeight: FontWeight.bold, color: black);
+TextStyle subtitle = TextStyle(fontWeight: FontWeight.w400, color: grey);
