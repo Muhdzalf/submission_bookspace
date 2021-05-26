ba0ffcf2a4c616b3dfa2d78b11edbf85576a5ed1
Muhammad Dzalfiqri Sa'bani <muhdzalfikri@gmail.com>
2021-05-27 02:04:47 +0700

    add models

---
+++ lib/models/buku_lainnya.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/foundation.dart';
+
+class OtherBook {
+  String judul;
+  String penulis;
+  String image;
+  String ranting;
+  String sinopsis;
+
+  OtherBook(
+      {@required this.judul,
+      @required this.penulis,
+      @required this.ranting,
+      @required this.image,
+      @required this.sinopsis});
+}
+
+var otherbook = [
+  OtherBook(
+      judul: 'Dunia Sophie',
+      penulis: 'Joestin Gaarder',
+      ranting: '3.94',
+      image:
+          'https://cdn.gramedia.com/uploads/items/9786024410209_dunia-sophie-republish.jpg',
+      sinopsis:
+          'Sophie, seorang pelajar sekolah menengah berusia empat belas tahu. Suatu hari sepulang sekolah, dia mendapat sebuah surat misterius yang hanya berisikan satu pertanyaan: “Siapa kamu?”/nBelum habis keheranannya, pada hari yang sama dia mendapat surat lain yang bertanya: “Dari manakah datangnya dunia?”\nSeakan tersentak dari rutinitas hidup sehari-hari, surat-surat itu mempuat Sophie mulai mempertanyakan soal-soal mendasar yang tak pernah dipikirkannya selama ini. Dia mulai belajar filsafat.'),
+  OtherBook(
+      judul: 'Bumi Manusia',
+      penulis: 'Pramudya Ananta Toer',
+      ranting: '4.40',
+      image: 'https://cdn.gramedia.com/uploads/items/bumi-manusia-edit.jpg',
+      sinopsis:
+          'Roman Tetralogi Buru mengambil latar belakang dan cikal bakal nation Indonesia di awal abad ke-20. Dengan membacanya waktu kita dibalikkan sedemikian rupa dan hidup di era membibitnya pergerakan nasional mula-mula, juga pertautan rasa, kegamangan jiwa, percintaan, dan pertarungan kekuatan anonim para srikandi yang mengawal penyemaian bangunan nasional yang kemudian kelak melahirkan Indonesia modern.\nRoman bagian pertama; Bumi Manusia, sebagai periode penyemaian dan kegelisahan dimana Minke sebagai aktor sekaligus kreator adalah manusia berdarah priyayi yang semampu mungkin keluar dari kepompong kejawaannya menuju manusia yang bebas dan merdeka, di sudut lain membelah jiwa ke-Eropa-an yang menjadi simbol dan kiblat dari ketinggian pengetahuan dan peradaban.'),
+  OtherBook(
+      judul: 'Muda Berdaya Karya Raya',
+      penulis: 'Fahd Pahdepie',
+      ranting: '4.02',
+      image:
+          'https://cdn.gramedia.com/uploads/items/9786025734724_muda-berdaya-.jpg',
+      sinopsis:
+          'Konon, fase terberat dalam hidup adalah usia dua puluhan. Jika seseorang berhasil melewati fase ini dengan baik, Hidup akan abik-baik saja hingga akhir. Jika gagal, segalanya menjadi lebih berat lagi di fase berikutnya.\nDi usia 20-an dan awal 30-1n, setiap orang mengalami krisis hebat. tentang penemuan jati diri. Tentang menjadi cocok dengan lingkungan . Tentang menemukan kompas untuk melangkah ke masa depan. Sementarakita sibuk di anatara dilema: bersenang-senang atau bekerja keras? Pacaran dulu atau menikah saja? Bekerja ataua berwirausaha? Memnajakan diri atau berinvestasi? Lebih penting membahagiakan diri sendiri dulu atau orangtua dan keluarga?'),
+  OtherBook(
+      judul: 'Ubah Lelah Jadi Lillah',
+      penulis: 'Dwi Suwiknyo',
+      ranting: '4.28',
+      image:
+          'https://cdn.gramedia.com/uploads/items/Ubah_Lelah_Jadi_Lillah.jpg',
+      sinopsis:
+          'Terkadang untuk mengejar satu target tertentu, ada bagian dari hidup kita yang harus dikorbankan. Demi target pekerjaan misalnya, keluarga terabaikan, kesehatan kita tak terpikirkan, dan bahkan hak Allah pun terlewatkan. Padahal itu hanya ketidakmampuan kita mengelola waktu, atau memang tidak ada niatan untuk menyeimbangkan hidup?\nSemoga segala aktivitas kita sehari-hari tidak sia-sia. Segala lelah bisa berbuah bahagia, tidak hanya bahagia untuk diri kita sendiri, melainkan juga untuk orang-orang terdekat. Semoga apa pun yang kita cintai di dunia ini, sama sekali tidak bisa memalingkan hati kita dari Allah . Untuk itulah buku ini dihadirkan sebagai teman diskusi, sebagai bahan renungan untuk saling mengingatkan betapa sayang sekali jika kita melewatkan hidup ini dengan sia-sia dan tidak tentu arah. Lantas apa yang coba ditawarkan buku ini?'),
+  OtherBook(
+      judul: 'Rapijali',
+      penulis: 'Dee Lestari',
+      ranting: '3.88',
+      image: 'https://cdn.gramedia.com/uploads/items/9786022917724.jpeg',
+      sinopsis:
+          'Ping merasa telah memiliki segala yang ia butuhkan. Dunianya yang damai di Pantai Batu Karas,rumahnya yang penuh alat musik di tepi Sungai Cijulang, seorang sahabat terbaik, serta kakek yang menyayanginya. Namun, diam-diam Ping menyimpan kegelisahan tentang masa depannya yang buram. Bakat musiknya yang istimewa tidak memiliki wadah, dan ia tidak berani bercita-cita. \n Hidup Ping jungkir balik ketika ia harus pindah ke Jakarta dan tinggal bersama keluarga calon gubernur. Ping mesti menghadapi sekolah baru, kawan-kawan baru, dan tantangan baru. Mungkinkah ia menemukan apa yang hilang selama ini? Dan, apakah Ping siap dengan yang ia temukan? Bahwa, hidupnya ternyata tidak sesederhana yang ia duga.'),
+];
---
+++ lib/models/buku_rekomendasi.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+
+class RecomendBook {
+  String judul;
+  String penulis;
+  String image;
+  String ranting;
+  String sinopsis;
+
+  RecomendBook(
+      {@required this.judul,
+      @required this.penulis,
+      @required this.ranting,
+      @required this.image,
+      @required this.sinopsis});
+}
+
+var recomendBook = [
+  RecomendBook(
+      judul: 'Bicara itu ada Seninya',
+      penulis: 'Oh Syu Hyang',
+      ranting: '',
+      image: 'https://cdn.gramedia.com/uploads/items/9786024553920_BDgfNGw.jpg',
+      sinopsis:
+          '"TAHUKAH ANDA BAHWA BERBICARA ITU ADA SENINYA?\nKetika komunikasi menjadi hal yang penting untuk bersaing, pakar komunikasi Oh Su Hyang mengeluarkan buku yang sangat berarti. Selain berisi tentang pengalaman peningkatan diri, buku ini juga memuat berbagai konten mengenai teknik komunikasi, persuasi, dan negosiasi.\nLalu bagaimana cara berbicara yang baik? Apakah berbicara dengan artikulasi yang jelas? Atau berbicara tanpa mengambil tarikan napas? Tidak! Sebuah ucapan yang bisa disebut baik adalah yang bisa menggetarkan hati. Ucapan seorang juara memiliki daya tarik tersendiri. Ucapan pemandu acara memiliki kemampuan menghidupkan suasana dan kekuatan kalimatnya yang terus terang. Anda harus pandai berbicara untuk menunjukkan diri Anda kepada lawan bicara dalam kehidupan sosial. Orang yang berbicara dengan mahir akan menjadi lebih maju daripada yang lainnya.\nUntuk mencapai tujuan komunikasi, persuasi, dan negosiasi, Anda harus mengetahui metode komunikasi yang efisien. Buku ini dijabarkan agar dapat dimengerti oleh siapa saja. Terdapat banyak episode menarik dari orang-orang terkenal dan juga rahasia inti dari komunikasi. Jika Anda membacanya dengan runut, saya yakin rasa percaya diri Anda untuk berbicara pun akan tumbuh dengan sendirinya."'),
+  RecomendBook(
+      judul: 'Sebuah Seni untuk Bersikap Bodo Amat',
+      penulis: 'Mark Manson',
+      ranting: '3.93',
+      image:
+          'https://cdn.gramedia.com/uploads/items/9786024526986_Sebuah-Seni-Untuk-Bersikap-Bodo-Amat.jpg',
+      sinopsis:
+          '"Selama beberapa tahun belakangan, Mark Manson—melalui blognya yang sangat populer—telah membantu mengoreksi harapan-harapan delusional kita, baik mengenai diri kita sendiri maupun dunia. Ia kini menuangkan buah pikirnya yang keren itu di dalam buku hebat ini.\n“Dalam hidup ini, kita hanya punya kepedulian dalam jumlah yang terbatas. Makanya, Anda harus bijaksana dalam menentukan kepedulian Anda.” Manson menciptakan momen perbincangan yang serius dan mendalam, dibungkus dengan cerita-cerita yang menghibur dan “kekinian”, serta humor yang cadas. Buku ini merupakan tamparan di wajah yang menyegarkan untuk kita semua, supaya kita bisa mulai menjalani kehidupan yang lebih memuaskan, dan apa adanya."'),
+  RecomendBook(
+      judul: 'Senu untuk Mengenal Diri Sendiri',
+      penulis: 'Brian Adam',
+      ranting: '-',
+      image:
+          'https://cdn.gramedia.com/uploads/items/seni_mengenal_diri_sendiri.jpg',
+      sinopsis:
+          'Seni Mengenal Diri Sendiri, seri seni hidup selanjutnya yang siap untuk kalian baca. Buku ini merupakan catatan dan panduan bagi Anda tentang cara mengenal dan mengelola diri sendiri. Dengan menerapkan pedoman di dalamnya, maka Anda dapat segera bertindak untuk mengenal diri sendiri dan kemauan Anda yang sebenarnya. Kejenuhan dalam menjalani hidup sangat mungkin terjadi karena Anda tidak benar-benar mengenal diri sendiri. Anda juga merasa tidak bersemangat karena Anda tidak menyadari tujuan hidup Anda.'),
+  RecomendBook(
+      judul: 'Kami (Bukan) Sarjana Kertas',
+      penulis: 'Js. Khairen',
+      ranting: '-',
+      image: 'https://cdn.gramedia.com/uploads/items/9786022203858.jpg',
+      sinopsis:
+          'Di Kampus UDEL, terjebaklah tujuh mahasiswa yang hidup segan kuliah tak mau. Mereka terpaksa kuliah di kampus yang google saja tak dapat mendeteksi. Cobalah sekarang Anda googLing "Kampus UDEL", takkan bertemu!\nAlasan mereka masuk UDEL macam-macam. Ada yang otaknya tak mampu masuk negeri, ada yang uang orangtuanya tak cukup masuk swasta unggul, ada pula yang karena.. biar kuliah aja.'),
+];
